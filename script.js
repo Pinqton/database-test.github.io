a4c62f985842f7cae16a22bc4be819a5a5d63b26
Pinqton <blackrose2271@gmail.com>
2025-08-31 11:39:17 -0400

    made code neater

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ const pass = "world";
 
 const label = document.getElementById("message");
 
+function showSuccess() {
+    label.innerText = "Logged in successfully";
+    label.style.backgroundColor = "#3ff374";
+}
+
+function showFailure() {
+    label.innerText = "Login failed";
+    label.style.backgroundColor = "#ed5a5a";
+}
+
 function login_clicked() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    label.hidden = false;
-
-    if (username === user && password === pass) {
-        label.innerText = "Logged in successfully";
-        label.style.backgroundColor = "#3ff374";
-        label.style.color = "black";
-    } else {
-        label.innerText = "Login failed";
-        label.style.backgroundColor = "#ed5a5a";
-        label.style.color = "black";
-    }
+    if (label.hidden) label.hidden = false;
+    if (username === user && password === pass) showSuccess(); else showFailure();
 }
